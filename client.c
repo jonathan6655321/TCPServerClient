06cb61d7c88ffa73a98249478a255860abe87640
Jonathan Somer <somer@mail.tau.ac.il>
2017-06-17 22:18:59 +0300

    comments deleted

--- client.c
+++ client.c
@@ -62,32 +62,6 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
-//int getInput(unsigned int numCharsToWrite, char **buf)
-//{
-//	*buf = (char *) malloc((numCharsToWrite + 1)*sizeof(char));
-//	if (*buf == NULL)
-//	{
-//		printf("Error: malloc for buffer failed\n");
-//		return -1;
-//	}
-//
-//	int fd = open(INPUT_ADDRESS, O_RDONLY);
-//	if (fd < 0)
-//	{
-//		printf("Error: failed to open file for reading\n");
-//		return -1;
-//	}
-//
-//	if (read(fd, *buf, numCharsToWrite) != numCharsToWrite)
-//	{
-//		printf("Error: failed to read from file\n");
-//		return -1;
-//	}
-//
-//	(*buf)[numCharsToWrite] = '\0';
-//	close(fd);
-//	return 1;
-//}
 
 void initSockAddr(struct sockaddr_in *addr)
 {
